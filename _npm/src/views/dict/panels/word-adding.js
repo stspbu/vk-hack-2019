1f80bc9aa3d39e2a668a5af9203130126b2e9191
stspbu <stardust.skg@gmail.com>
2019-09-28 01:54:52 +0300

    Смерджил WordAdding

--- _npm/src/views/dict/panels/word-adding.js
+++ _npm/src/views/dict/panels/word-adding.js
@@ -1,14 +1,45 @@
 import * as React from "react";
-import {BaseComponent, DataLoader} from "../../../base";
-import {Panel, PanelHeader} from "@vkontakte/vkui";
+import {BaseComponent} from "../../../base";
+import {Panel, PanelHeader, HeaderButton, Input, Button, Div} from "@vkontakte/vkui";
+
+import Icon24Back from '@vkontakte/icons/dist/24/back';
+
 
 class WordAddingPanel extends BaseComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            word: ''
+        }
+    }
+
+    onChange(e) {
+        let word = e.target.value;
+        this.log('On change a word: ' + word);
+
+        this.setState({
+            word: word
+        })
+    }
+
+    onClick() {
+        // см dict.js для примера перехода
+    }
+
     render() {
         return (
             <Panel id="word_adding_panel">
-                <PanelHeader>
+                <PanelHeader
+                    left={<HeaderButton><Icon24Back/></HeaderButton>}>
                     Добавление слова
                 </PanelHeader>
+                <Input value={this.state.word} onChange={this.onChange.bind(this)}/>
+                <Div align="center">
+                    <Button onClick={this.onClick.bind(this)}>
+                        GO!
+                    </Button>
+                </Div>
             </Panel>
         )
     }
