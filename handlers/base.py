30399ca4979c61a0ef4f9ded8e94bc1201c6019e
danila19991 <guseffdanila@ya.ru>
2019-09-27 22:04:01 +0300

    add sign checks

--- handlers/base.py
+++ handlers/base.py
@@ -1,9 +1,24 @@
+import logging
+
+from base64 import b64encode
+from collections import OrderedDict
+from hashlib import sha256
+from hmac import HMAC
+from urllib.parse import urlencode
 import tornado.web
+import json
+
+
+client_secret = 'PqwFekrI1arHHZwXKk1w'
 
 
 class BaseHandler(tornado.web.RequestHandler):
     def prepare(self):
+        logging.warning(self.request.arguments)
+        vk_subset = OrderedDict(sorted(x for x in self.request.arguments.items() if x[0][:3] == "vk_"))
+        hash_code = b64encode(HMAC(client_secret.encode(), urlencode(vk_subset, doseq=True).encode(), sha256).digest())
+        decoded_hash_code = hash_code.decode('utf-8')[:-1].replace('+', '-').replace('/', '_')
         sign = self.get_argument('sign')
 
-        if not True:    # TODO: validate signature
+        if sign != decoded_hash_code:
             self.send_error(403)
