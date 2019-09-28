9fccd3bd1f275873cfb4b31e049706ca1c551d29
tursunova <tursunovamunira@gmail.com>
2019-09-28 14:41:10 +0300

    tests in progress

--- _npm/src/views/test/panels/test.js
+++ _npm/src/views/test/panels/test.js
@@ -1,13 +1,58 @@
 import * as React from "react";
-import {CellButton, Group, List, PanelHeader, HeaderButton, Panel, Radio, Div, FormLayout} from "@vkontakte/vkui";
+import {CellButton, Group, List, PanelHeader, HeaderButton, Panel, Radio, Div, FormLayout, Button} from "@vkontakte/vkui";
 
 import {BaseComponent, DataLoader} from "../../../base";
 
 const test_words = [
     {'answer': 0, 'word': 'hi', 'variants': ['привет', 'пока', 'здравствуй', 'да']},
     {'answer': 1, 'word': 'fuck', 'variants': ['привет', 'пока', 'здравствуй', 'да']}
-]
+];
 
+class TaskItem extends BaseComponent{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            task: this.props.task
+        };
+    }
+    //
+    // getScore(){
+    //
+    // }
+
+    render(){
+        return (
+            <Group>
+                <List>
+                    {
+                        this.state.task.map((task) =>
+                            <FormLayout>
+                                <Div>
+                                    <Div>
+                                        {task.word}
+                                    </Div>
+                                    <Radio name="radio" value="1">
+                                        {task.variants[0]}
+                                    </Radio>
+                                    <Radio name="radio" value="1">
+                                        {task.variants[1]}
+                                    </Radio>
+                                    <Radio name="radio" value="1">
+                                        {task.variants[2]}
+                                    </Radio>
+                                    <Radio name="radio" value="1">
+                                        {task.variants[3]}
+                                    </Radio>
+                                </Div>
+                            </FormLayout>
+                        )}
+                </List>
+            </Group>
+        )
+    }
+
+}
 
 class Tasks extends BaseComponent {
     constructor(props) {
@@ -20,32 +65,16 @@ class Tasks extends BaseComponent {
     }
     render() {
         return (
-            <Group>
-                <List>
-                    {
-                        this.state.words.map((word) =>
-                            <FormLayout>
-                                <Div>
-                                    <Div>
-                                        {word.word}
-                                    </Div>
-                                    <Radio name="radio" value="1">
-                                        {word.variants[0]}
-                                    </Radio>
-                                    <Radio name="radio" value="1">
-                                        {word.variants[1]}
-                                    </Radio>
-                                    <Radio name="radio" value="1">
-                                        {word.variants[2]}
-                                    </Radio>
-                                    <Radio name="radio" value="1">
-                                        {word.variants[3]}
-                                    </Radio>
-                                </Div>
-                            </FormLayout>
-                    )}
-                </List>
-            </Group>
+            <Div>
+                <TaskItem
+                    task = {this.state.words}
+                />
+                <Div align="center">
+                    <Button>
+                        Finish!
+                    </Button>
+                </Div>
+            </Div>
         )
     }
 }
@@ -55,11 +84,14 @@ class TestPanel extends BaseComponent {
     render() {
         return (
             <Panel id="test_panel">
-                <PanelHeader>
+                <PanelHeader
+                    left={<HeaderButton onClick={this.goBack.bind(this)}><Icon24Back/></HeaderButton>}
+                >
                     Тест
                 </PanelHeader>
                 <Tasks/>
-                //words from server
+
+                {/*words from server*/}
                 {/*<DataLoader*/}
                     {/*endpoint='/tests/'*/}
                     {/*loaded={*/}
