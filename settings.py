0d6c5adb0d0efb6f706572786e1e46ed08884022
danila19991 <guseffdanila@ya.ru>
2019-09-28 12:10:36 +0300

    move secret_key to settings

--- settings.py
+++ settings.py
@@ -4,6 +4,14 @@ settings = {
     'DB_PASS': 'postgres',
     'DB_HOST': 'localhost',
     'DB_PORT': '5432',
-    'DB_NAME': 'sdict'
+    'DB_NAME': 'sdict',
+    'SECRET_KEY': 'PqwFekrI1arHHZwXKk1w'
 }
+
+try:
+    import local_settigs
+    for key, val in local_settigs.values():
+        settings[key] = val
+except ImportError:
+    pass
 
